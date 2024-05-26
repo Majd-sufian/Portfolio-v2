c509abb8b504b4686a0bf2aba171185b36167efd
Majd-sufyan <majdnewsufian@gmail.com>
2024-05-26 21:09:35 +0200

    Update metadata in RootLayout component 🌎

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,16 @@ import { ThemeProvider } from "./provider";
 
 const inter = Inter({ subsets: ["latin"] });
 
+export const metadata = {
+  title:
+    "Majd Sufyan | Full Stack Developer specializing in React, Node, AWS and more...",
+  description:
+    "I am Majd Sufyan a full stack developer specializing in React, Node, AWS, and more. With a passion for turning fantastic ideas into reality, I bring creativity and expertise to every project.",
+  url: "https://majd-sufyan.site",
+  image:
+    "https://camo.githubusercontent.com/81c235b7d2fa5f487d5f146fbb0ba653b42da982d613680ecca41c2f24082981/68747470733a2f2f692e706f7374696d672e63632f344a6a6276566e532f53637265656e73686f742d323032342d30352d32352d61742d32312d33312d34312e706e67",
+};
+
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -13,45 +23,6 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <Head>
-        <link rel="shortcut icon" href="/favicon.ico" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>
-          Majd Sufyan | Full Stack Developer specializing in React, Node, AWS
-          and more...
-        </title>
-        <meta
-          name="description"
-          content="I am Majd Sufyan a full stack developer specializing in React, Node, AWS, and more. With a passion for turning fantastic ideas into reality, I bring creativity and expertise to every project."
-        />
-        <meta property="og:type" content="website" />
-        <meta
-          property="og:title"
-          content="Majd Sufyan | Full stack Developer specializing in React, Node, AWS, and more"
-        />
-        <meta property="og:url" content="https://majd-sufyan.site" />
-        <meta
-          property="og:image"
-          content="https://github.com/Majd-sufian/Portfolio/blob/main/public/png/portfolio.png?raw=true"
-        />
-        <meta
-          property="og:description"
-          content="I am Majd Sufyan a full stack developer specializing in React, Node, AWS, and more. With a passion for turning fantastic ideas into reality, I bring creativity and expertise to every project."
-        />
-        <meta
-          name="twitter:title"
-          content="Majd Sufyan | Full stack Developer specializing in React, Node, AWS, and more"
-        />
-        <meta
-          name="twitter:description"
-          content="I am Majd Sufyan is a full stack developer specializing in React, Node, AWS, and more. With a passion for turning fantastic ideas into reality, I bring creativity and expertise to every project."
-        />
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta
-          name="twitter:image"
-          content="https://github.com/Majd-sufian/Portfolio/blob/main/public/png/portfolio.png?raw=true"
-        />
-      </Head>
       <body className={inter.className}>
         <ThemeProvider
           attribute="class"
