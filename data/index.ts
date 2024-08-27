944cf33b60417ffea6b903729d8c9747f6efa1b8
Majd-sufyan <majdnewsufian@gmail.com>
2024-08-27 23:09:13 +0200

    removed blog descriptions 🗑️

--- data/index.ts
+++ data/index.ts
@@ -257,48 +257,36 @@ export const blogs: Array<Blog> = [
   // state
   {
     title: "Managing React State Like A Superhero 🦸",
-    description:
-      "The state is an important concept in React, as it allows components to store and manage data that can change over time. Properly managing state can help make your React application more efficient and easier to maintain. In this article, we will explore different ways to manage state in React and provide code examples for each method.",
     link: "https://dev.to/majdsufian/managing-react-state-like-a-superhero-5gnp",
     img: "https://me-portfolio.s3.eu-central-1.amazonaws.com/superhero.png",
   },
   // filter
   {
     title: "Filter(Boolean) trick 💡",
-    description:
-      "One use case for the filter() method is to remove falsy values from an array. You can do this by using the Boolean function as the callback for the filter() method.",
     link: "https://dev.to/majdsufian/you-should-know-aobut-the-filterboolean-trick-4g0",
     img: "https://me-portfolio.s3.eu-central-1.amazonaws.com/filter.png",
   },
   // react cheatsheet
   {
     title: "React Cheatsheet for 2023 ✍️",
-    description:
-      "Welcome to my React cheat sheet! In this article, I'll cover all the common concepts and techniques we use every day when developing in React. I'll be covering topics such as React elements, element attributes, element styles, fragments, components, props, children props, conditionals, lists, context, and hooks.",
     link: "https://dev.to/majdsufian/react-cheatsheet-for-2023-14bd",
     img: "https://me-portfolio.s3.eu-central-1.amazonaws.com/react-cheatsheet.png",
   },
   // google
   {
     title: "Google like a pro 🕵️‍♀️",
-    description:
-      "Googling is one of the most important skills for every developer,so let Let me show you how to get better at Googling to get faster results",
     link: "https://dev.to/majdsufian/google-is-your-best-friend-so-use-it-like-a-pro-1o88",
     img: "https://me-portfolio.s3.eu-central-1.amazonaws.com/google.gif",
   },
   // json
   {
     title: "One Tool to view your Json better 👨‍💻",
-    description:
-      "𝐉𝐬𝐨𝐧𝐂𝐫𝐚𝐜𝐤 generates graph diagrams from JS ON files and makes them easy to read. Additionally, the generated diagrams can be downloaded as images.",
     link: "https://medium.com/@majdnewsufian/one-tool-to-view-your-json-better-1cce7b3633c3",
     img: "https://me-portfolio.s3.eu-central-1.amazonaws.com/json.png",
   },
   // grepper
   {
     title: "One tool can save time when googling ⏲",
-    description:
-      "Whenever you search online for anything related to programming, if it has an answer to your search, it displays snippets of code or information based on what you have searched.",
     link: "https://medium.com/@majdnewsufian/one-tool-can-save-time-when-googling-2366062430a6",
     img: "https://me-portfolio.s3.eu-central-1.amazonaws.com/grepper.png",
   },
